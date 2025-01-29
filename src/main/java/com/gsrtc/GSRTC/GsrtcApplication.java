202f2e7db1538c8ac9d6380011cb91568880574f
Zeel <zeeldesai2163@gmail.com>
2025-01-29 23:02:45 +0530

    Added Basic Structure

---
+++ src/main/java/com/gsrtc/GSRTC/GsrtcApplication.java
@@ -0,0 +1,13 @@
+package com.gsrtc.GSRTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsrtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsrtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsrtc/GSRTC/GsrtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsrtc.GSRTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsrtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
